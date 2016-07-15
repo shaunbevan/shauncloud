c7aae9cf5b7a5bc3ac8b7b5d5c6979f28a8495d2
shaunbevan <tbevan@gmail.com>
2016-07-15 13:31:18 -0400

    Initial Commit

---
+++ shauncloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shauncloud
+//
+//  Created by Shaun on 7/15/16.
+//  Copyright © 2016 Shaun Bevan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
