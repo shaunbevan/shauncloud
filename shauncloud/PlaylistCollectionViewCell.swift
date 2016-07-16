db6c7a5f49081a10ecff0c23aa3bb989084c79ce
shaunbevan <tbevan@gmail.com>
2016-07-16 13:41:59 -0400

    Added KeychainAccess, Setup Network layer

---
+++ shauncloud/PlaylistCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlaylistCollectionViewCell.swift
+//  shauncloud
+//
+//  Created by Shaun on 7/16/16.
+//  Copyright © 2016 Shaun Bevan. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistCollectionViewCell: UICollectionViewCell {
+
+}
--- shauncloud/PlaylistViewController.swift
+++ shauncloud/PlaylistViewController.swift
@@ -10,33 +10,34 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class PlaylistViewController: UIViewController {
+class PlaylistViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let networking = Networking()
+    
+    private var cellIdentifier: String = "Cell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let userEndpoint: String = "http://api.soundcloud.com/users/\(id)?client_id=\(key)"
-//        
-//        Alamofire.request(.GET, userEndpoint)
-//            .responseJSON { response in
-//                guard response.result.error == nil else {
-//                    // Got an error getting the data, handle it
-//                    print("Error: \(response.result.error!)")
-//                    return
-//                }
-//                
-//                if let value = response.result.value {
-//                    let user = JSON(value)
-//                    
-//                    print("The user is: \(user)")
-//                    
-//                    if let username = user["username"].string {
-//                        print("The username is: \(username)")
-//                    } else {
-//                        print("Error parsing users")
-//                    }
-//                }
-//        }
     }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = self.collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! PlaylistCollectionViewCell
+        
+        cell.backgroundColor = UIColor.greenColor()
+        return cell
+    }
+    
+    
 
 }
